bd34a64fbff261708c8fbcb4a87c8b6084cff9b4
Jay_Makwana <jaimakwana786@gmail.com>
2020-10-21 12:09:01 +0530

    add icon dropdown

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -23,6 +23,11 @@ Vue.use(SpaceWind, { theme })
 require('./components/base')
 
 /**
+ * Icon Components
+ */
+require('./components/icons')
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
