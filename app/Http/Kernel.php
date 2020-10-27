4928c352bdadb6f3a8f01dd1db7a94c7e5f44ce4
jayvirsinh_gohil <gohiljsyvirsinh02@gmail.com>
2020-10-27 15:16:36 +0530

    add redirect-if-unauthenticated

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'redirect-if-unauthenticated' => \App\Http\Middleware\RedirectIfUnauthorized::class,
     ];
 }
